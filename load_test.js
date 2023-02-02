75c997bf023a3fad8988cb172db3ee32221b070f
Sumit Arora <er.sumitarora@gmail.com>
2023-02-01 21:33:37 -0500

    updating tests

--- load_test.js
+++ load_test.js
@@ -14,13 +14,13 @@ export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     stages: [
-        { duration: '1m', target: 100 }, // ramp up users to 100 in 1 minute
-        { duration: '3m', target: 100 }, // stay at 100 users for 3 minutes
-        { duration: '2m', target: 0 }, // ramp down to 0 users in 2 minutes
+        { duration: '5s', target: 100 }, // ramp up users to 100 in 1 minute
+        { duration: '10s', target: 200 }, // stay at 100 users for 3 minutes
+        { duration: '5m', target: 0 }, // ramp down to 0 users in 2 minutes
 
     ],
     thresholds: {
-        http_req_duration: ['p(99)<150'], // 99% of requests should complete within 150ms
+        http_req_duration: ['p(95)<150'], // 99% of requests should complete within 150ms
     }, 
 };
 
